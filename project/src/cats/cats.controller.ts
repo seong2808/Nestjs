2efa7e9b8a98dcd7e709bf1de5e071d5b63e4e8e
seongkwan <seong2808@gmail.com>
2023-11-19 02:00:10 +0900

    feat: Exception filter & Pipes

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
+// class 전역에 사용 가능
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   // cats/
   @Get()
+  // 아래 throw new HttpException에서 발생된 Exception이 UseFilters를 통해 HttpExceptionFilter에 필터링되어 response에 전달이 된다.
+  //   @UseFilters(HttpExceptionFilter) --> class 안의 일부분일 떄 사용
   getAllCat() {
-    return 'all cat';
+    // throw new HttpException({ success: false, message: 'api is broken' }, 401);
+    throw new HttpException('api is broken', 401);
+    return 'get all cat api';
   }
   // cats/:id
   @Get(':id')
-  getOneCat() {
-    return 'one cat';
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
+    return 'get one cat api';
   }
   // cats/
   @Post()
   createCat() {
-    return 'create cat';
+    return 'create cat api';
   }
   // cats/:id
   @Put(':id')
   updateCat() {
-    return 'update cat';
+    return 'update cat api';
   }
   // cats/:id
   @Patch(':id')
   updatePartialCat() {
-    return 'update';
+    return 'update partial cat api';
   }
   // cats/:id
   @Delete(':id')

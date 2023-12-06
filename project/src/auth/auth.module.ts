1f0453c6c3af8fc624839950ab64bba284b63443
seongkwan <seong2808@gmail.com>
2023-12-06 22:55:13 +0900

    feat: Swagger API 보안 설정

--- project/src/auth/auth.module.ts
+++ project/src/auth/auth.module.ts
@@ -4,14 +4,16 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { CatsModule } from 'src/cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     // strategy에 대해서 기본적인 설정할 수 있다.
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     // 로그인할 때 사용 예정
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1y' },
     }),
 

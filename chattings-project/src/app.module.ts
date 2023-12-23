f5130c2cff3597b91086d35b72b6428656d4c8fa
seongkwan <seong2808@gmail.com>
2023-12-23 23:00:05 +0900

    feat: 소켓을 사용하기 위한 세팅

--- chattings-project/src/app.module.ts
+++ chattings-project/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsModule } from './chats/chats.module';
 import mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    ChatsModule,
   ],
   controllers: [AppController],
   providers: [],

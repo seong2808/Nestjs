b3fcf6139def19b7e12cdb3ae60512b2ddd7a478
seongkwan <seong2808@gmail.com>
2023-12-24 13:52:36 +0900

    feat: emit&on

--- chattings-project/public/js/scripts.js
+++ chattings-project/public/js/scripts.js
@@ -9,6 +9,15 @@ const formElement = getElementById('chat_form');
 
 function helloUser() {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    console.log(data); // return으로 받은 data
+  });
+
+  console.log(`name: ${username}`);
+
+  socket.on('hello_user', (data) => {
+    console.log(data); // 서버에서 emit으로 보낸 data
+  });
 }
 
 function init() {
--- chattings-project/src/chats/chats.gateway.ts
+++ chattings-project/src/chats/chats.gateway.ts
@@ -1,9 +1,22 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('new_user')
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(socket.id);
+    console.log(username);
+
+    socket.emit('hello_user', 'hello ' + username);
+    return 'hello world!';
   }
 }

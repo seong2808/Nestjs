850726ebc3a4c4e956b037bbba15fcbb83b8ecf5
seongkwan <seong2808@gmail.com>
2024-01-08 22:33:09 +0900

    feat: namespace 생성 및 Gateway lifecycle hooks 사용

--- chattings-project/src/chats/chats.gateway.ts
+++ chattings-project/src/chats/chats.gateway.ts
@@ -1,13 +1,36 @@
+import { Logger } from '@nestjs/common';
 import {
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayInit,
   MessageBody,
   ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`Disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`Connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
